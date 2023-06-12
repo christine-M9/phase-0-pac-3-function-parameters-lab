640137adbf17e410af1679bd75fc93f3dd563233
Christine <christinekhekama99@gmail.com>
2023-06-12 01:18:03 -0700

    complete assignment

--- index.js
+++ index.js
@@ -1,12 +1,8 @@
-function introduction(name) {
-    return 'Hi, my name is Aki.';
+function introduction(name){
+    return (`Hi, my name is ${name}.`);
 }
-introduction(Aki);
-introduction(Aki, Ember.js)
-function introductionWithLanguage(name, language) {
-    return 'Hi, my name is {Aki} and I am learning to program in {Ember.js}.'
-}
-function introductionWithLanguageOptional(name,language ="JavaScript") {
-    return 'Hi, my name is Gracie and I am learning to program in JavaScript.';
-    introductionWithLanguageOptional(Gracie);
+introduction("Aki");
+
+function introductionWithLanguage(name, language){
+    return (`Hi, my name is ${name} and I am learning to program in ${language}.`)
 }
