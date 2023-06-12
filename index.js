bf60ec49f4cabb989c4388834ff782fc0325274f
Christine <christinekhekama99@gmail.com>
2023-06-12 01:32:23 -0700

    complete assignment

--- index.js
+++ index.js
@@ -6,3 +6,8 @@ introduction("Aki");
 function introductionWithLanguage(name, language){
     return (`Hi, my name is ${name} and I am learning to program in ${language}.`)
 }
+introductionWithLanguage("Aki", "Javascript")
+function introductionWithLanguageOptional(name, language = "JavaScript"){
+    return (`Hi, my name is ${name} and I am learning to program in ${language}.`);
+}
+introductionWithLanguageOptional("Aki", "JavaScript");
